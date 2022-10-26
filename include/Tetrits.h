60667a58649e6789b5279be964f7422cf1815d00
R <roseba@odroidc4>
2022-10-25 21:57:27 -0400

    Pretty good. Colision working pretty well. Still no collision with the walls.

--- include/Tetrits.h
+++ include/Tetrits.h
@@ -9,24 +9,33 @@
 #include "YanceyShape.h"
 #include "colors_config.h"
 
-
 #define PARENTGAME SDLGame
 #define WINDOWFLAGS SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK
-
-#define WIND_H 800
-#define WIND_W 400
 
 #define FRAMERATE 20
 #define BLOCKSIZE 20 // * 2
 
-#define SPAWN {200,60}
-//#define FALLS {0,-20}
+#define WIND_H (BLOCKSIZE * 2 * 21)
+#define WIND_W  (BLOCKSIZE * 2 * 11)
 
-#define LEFTMOVE Yancey_Vector({-1 , 0})
-#define RIGHTMOVE Yancey_Vector({1 , 0})
+#define BLOCK Yancey_Vector({BLOCKSIZE,BLOCKSIZE})
+
+#define SPAWN { (BLOCKSIZE * 10), (BLOCKSIZE * 3)}
+
+#define MOVE Yancey_Vector({1 , 0})
 
 #define GAMEBACKGROUND YANCEYCOLOR_DarkBlue
 #define BLOCKCOLOR YANCEYCOLOR_White
+
+class Tetrits_Block : public Yancey_shape2d{
+public:
+  Tetrits_Block();
+  Tetrits_Block(const Tetrits_Block &b);
+  ~Tetrits_Block();
+  Tetrits_Block(Yancey_Vector loc);
+  bool collides_with(Tetrits_Block other);
+  Yancey_Vector location;
+};
 
 class Tetromino 
 {
@@ -34,10 +43,11 @@ public:
   Tetromino(Yancey_Vector location, uint8_t shape);
   Tetromino();
   Tetromino(const Tetromino &t);
-  ~Tetromino(){};
+  virtual ~Tetromino(){};
   void rotate(bool cw);
+  bool lands(std::vector<Tetrits_Block> &f);
   Yancey_Vector location;
-  std::vector<Yancey_rect> blocks;
+  std::vector<Tetrits_Block> blocks;
   uint8_t shape;
   Yancey_Vector orientation;
 };
@@ -49,11 +59,21 @@ public:
   bool init(uint32_t flags);
   bool update() override;
   bool spawn(Tetromino& t);
+  void init_floor()
+  {
+  int j=this->wind_h;
+  for(int i = this->wind_w/2; i<=this->wind_w/2+BLOCKSIZE*2;i+=(BLOCKSIZE * 2))
+    this->floor.push_back(Tetrits_Block(Yancey_Vector({i,j}) ));
+  
+  };
   
   Tetromino active_tet;
   SDL_Joystick *joystick;
   std::vector<uint8_t> shapes = {'i','j','l','o','s','t','z'};
   Yancey_Vector  FALLS = {0,1};
+
+  std::vector<Tetrits_Block> floor = {  };
+
   
 //EVENTS
 
@@ -113,16 +133,25 @@ bool handle_key( SDL_KeyboardEvent *key )
       break;
     case SDLK_LEFT:
     case SDLK_a:
-      this->active_tet.location += LEFTMOVE * (2 * BLOCKSIZE);
+      this->active_tet.location -= MOVE * (2 * BLOCKSIZE);
       break;
     case SDLK_RIGHT:
     case SDLK_d:
-      this->active_tet.location += RIGHTMOVE * (2 * BLOCKSIZE);
+      this->active_tet.location += MOVE * (BLOCKSIZE);
       break;
+      /*
+ 
+    case SDLK_q:
+    case SDLK_UP:
+      this->active_tet.location -= this->FALLS * (BLOCKSIZE);
+      break;      
     case SDLK_s:
     case SDLK_DOWN:
       this->active_tet.location += this->FALLS * (2 * BLOCKSIZE);
-      break;      
+      this->active_tet.lands(this->floor);
+      //log_i<< (this->active_tet.location.y / BLOCKSIZE) <<" < "<< this->wind_h/BLOCKSIZE <<std::endl;
+      break;
+       */      
     case SDLK_w:
       this->active_tet.rotate(false);
       break;
--- src/Tetrits.cpp
+++ src/Tetrits.cpp
@@ -8,39 +8,88 @@ std::vector<Yancey_Vector> TETR_S = { {0,0}, {-2,0}, {0,-2}, {2,-2}};
 std::vector<Yancey_Vector> TETR_T = { {0,0}, {-2,0}, {2,0}, {0,-2}};
 std::vector<Yancey_Vector> TETR_Z = { {0,0}, {-2,0}, {0,2}, {2,2}};
 
+float BLOCKRAD = sqrt(BLOCKSIZE * BLOCKSIZE * 2);
+
+Tetrits_Block::Tetrits_Block(){}
+Tetrits_Block::Tetrits_Block(const Tetrits_Block &t):Tetrits_Block(t.location){}
+Tetrits_Block::~Tetrits_Block(){}
+
+Tetrits_Block::Tetrits_Block(Yancey_Vector loc):Yancey_shape2d(0,true,4,BLOCKRAD,loc)
+ {
+   this->location = loc;
+ }
+bool Tetrits_Block::collides_with(Tetrits_Block other)
+{
+  //log_i<< this->location.y <<" "<< other.location.y <<std::endl;
+  return (this->location.y + BLOCKSIZE) > (other.location.y - BLOCKSIZE);
+}
+
 Tetrits::Tetrits() : PARENTGAME("tetrits",WIND_W,WIND_H,FRAMERATE){}
 Tetrits::~Tetrits(){}
 bool Tetrits::init(uint32_t fl)
 {
   PARENTGAME::init(WINDOWFLAGS);
   std::srand(time(0));
+  this->init_floor();
   this->spawn(this->active_tet);
   return true;
 }
-bool Tetrits::spawn(Tetromino& t)
+bool Tetrits::spawn(Tetromino &t)
 {
+  //drop
+  for(Tetrits_Block b : t.blocks)
+    {
+      b.location = b.location * BLOCKSIZE + this->active_tet.location;
+      this->floor.push_back(b);
+    }
+  
   uint8_t sh = std::round(std::rand() % 7);
   t = Tetromino(SPAWN,this->shapes[sh]);
   return true;
+}
+
+bool Tetromino::lands(std::vector<Tetrits_Block> &floor)
+{
+  for(Tetrits_Block block : this->blocks)
+    {
+      block.location = block.location * BLOCKSIZE + this->location;
+      for(Tetrits_Block f : floor)
+	{
+	  log_i<<block.location.y<<std::endl;
+	  if(block.collides_with(f))
+	    {
+	    return true;
+	    }
+	}
+    }
+  return false;
 }
 bool Tetrits::update()
 {
   if(PARENTGAME::frames.ready)
     {
-       // log_i<< "upd" <<std::endl;
        this->render_clear( GAMEBACKGROUND );
        this->set_render_color(BLOCKCOLOR);
-
-       for(Yancey_rect block : this->active_tet.blocks)
+       
+       if(!this->active_tet.lands(this->floor))
+	 this->active_tet.location += this->FALLS * 9;
+       
+       for(Tetrits_Block block : this->active_tet.blocks)
 	 {
-	   block.location = block.location + this->active_tet.location;
-	   std::vector<Yancey_Vector> points = block.get_polygon();
-	   this->draw_lines(points);
+	   block.location = block.location * BLOCKSIZE + this->active_tet.location;
+	   this->draw_rectangle(block.location - BLOCK , 2 * BLOCKSIZE, 2 * BLOCKSIZE);
+	   //this->draw_rectangle(block.location - Yancey_Vector({1,1}) , 2, 2);
 	 }
        
        this->set_render_color(YANCEYCOLOR_Red);
-       this->draw_points({this->active_tet.location});
+       this->draw_rectangle(this->active_tet.location-Yancey_Vector({2,2}),4,4);
        
+       for(Tetrits_Block f : this->floor)
+	 {
+	   this->draw_rectangle(f.location - BLOCK, 2 * BLOCKSIZE, 2 * BLOCKSIZE);
+	   this->draw_rectangle(f.location - BLOCK * .5 , BLOCKSIZE, BLOCKSIZE);
+	 }
+	 
        this->render_present(0);
 
        PARENTGAME::frames.ready = false;
@@ -78,15 +127,15 @@ Tetromino::Tetromino(Yancey_Vector location, uint8_t shape):location(location),s
   default:
     break;
   }
-  for(Yancey_Vector b : locs)
-	 blocks.push_back(Yancey_rect(0,true,{BLOCKSIZE,BLOCKSIZE},b * BLOCKSIZE));
+  for(Yancey_Vector l : locs)
+    blocks.push_back(Tetrits_Block(l));
 }
-Tetromino::Tetromino():Tetromino(SPAWN,'l'){}
+Tetromino::Tetromino(){}//:Tetromino(SPAWN,'l'){}
 Tetromino::Tetromino(const Tetromino &t):location(t.location),shape(t.shape){}
 void Tetromino::rotate(bool cw)
 {
 this->orientation = this->orientation.get_normal(cw);
- for(Yancey_rect &b : this->blocks)
+ for(Tetrits_Block &b : this->blocks)
    b.location = b.location.get_normal(cw);
 }
     
